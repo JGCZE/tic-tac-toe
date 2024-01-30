eb5d4753ee8049ceb7058227b62062ff730f727c
Jan Gross <jan.gross@regiojet.cz>
2024-01-30 16:26:50 +0100

    delete components, change logic to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,25 @@
-//import { useState } from 'react'
+import { useState } from 'react'
 import './App.css'
-import Square from './components/Square'
 
 const App = () => {
-  //const [squares, setSquares] = useState<Array<number | string>>([])
+  const playFiled = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const [squares, setSquares] = useState<Array<number | string>>([])
+
+  const handleClick = (item: number) => {
+    console.log("clicked", item)
+  }
 
   return (
     <>
       <div className='container'>
         <div className='wrapper'>
           <div className='first-row'>
-            <Square value={1} />
-            <Square value={2} />
-            <Square value={3} />
-          </div>
-          <div className='second-row'>
-            <Square value={4} />
-            <Square value={5} />
-            <Square value={6} />
-          </div>
-          <div className='third-row'>
-            <Square value={7} />
-            <Square value={8} />
-            <Square value={9} />
+            {playFiled.map((item) => {
+              return (
+                <button key={item} value={item} className='square' onClick={() => handleClick(item)}/>
+              ) 
+            }
+            )}
           </div>
         </div>
 
