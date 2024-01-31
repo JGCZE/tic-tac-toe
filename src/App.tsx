53aaabee324b8b610d28ce6ce998b22a40baa5f4
Jan Gross <jan.gross@regiojet.cz>
2024-01-31 10:22:59 +0100

    logic for score X and Y player

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,27 @@ import './App.css'
 
 const App = () => {
   const playFiled = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-  const [squares, setSquares] = useState<Array<number | string>>([])
+  const [Xscore, setXScore] = useState<Array<number | string>>([])
+  const [Yscore, setYScore] = useState<Array<number | string>>([])
+  const [xIsNext, setXIsNext] = useState<boolean>(true)
 
   const handleClick = (item: number) => {
     console.log("clicked", item)
+    nextMove(item)
   }
+
+  const nextMove = (item:number) => {
+    if (xIsNext) {
+      setXScore([...Xscore, item])
+      setXIsNext(false)
+    } else {
+      setYScore([...Yscore, item])
+      setXIsNext(true)
+    }
+  }
+
+  console.log("score X: ", Xscore)
+  console.log("score Y: ", Yscore)
 
   return (
     <>
@@ -16,7 +32,9 @@ const App = () => {
           <div className='first-row'>
             {playFiled.map((item) => {
               return (
-                <button key={item} value={item} className='square' onClick={() => handleClick(item)}/>
+                <button key={item} value={item} className='square' onClick={() => handleClick(item)}>
+                  {}
+                </button>
               ) 
             }
             )}
